c4c8e152e501786ebbc1379c54e3b4e5938ea634
Seidy-u <uji02seidy01@gmail.com>
2024-10-02 18:04:48 +0900

    add entry point

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,9 +1,21 @@
-from django.views import generic
-from .forms import CustomUserCreationForm
+from numpy import generic
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from accounts.forms import CustomUserCreationForm
 from .models import CustomUser
+from .serializers import CustomUserSerializer 
 
-
-class CustomAccountCreationView(generic.CreateView):
-    model = CustomUser
-    form_class = CustomUserCreationForm
-    template_name = 'accounts/accounts_create.html'
+# class CustomAccountCreationView(generic.CreateView):
+#     model = CustomUser
+#     form_class = CustomUserCreationForm
+#     template_name = 'accounts/accounts_create.html'
+    
+@api_view(['POST'])
+def create_user(request):
+    if request.method == 'POST':
+        serializer = CustomUserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
