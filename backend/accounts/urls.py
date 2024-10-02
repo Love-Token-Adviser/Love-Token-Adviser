54951c1b19ed45ded819c4e87c6a95e8c397dfca
Sujan Shrestha <sujanshresstha@gmail.com>
2024-10-02 18:45:22 +0900

    created logout functionality

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
 
     path('login/', views.Login.as_view(), name='login'),
 
+    path('logout/', views.Logout.as_view(), name='logout'),
+    
     path('home/', login_required(views.Home.as_view()), name='home'),
 ]
