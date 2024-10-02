c9169df7ac934b072a3cf158fc58ee9c688466be
Sujan Shrestha <sujanshresstha@gmail.com>
2024-10-02 17:11:37 +0900

    login feature added

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import CustomAccountCreationView
+from .views import CustomAccountCreationView, Login
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('signup/', CustomAccountCreationView.as_view(), name='custom_accounts_create'),
+
+    path('login/', Login.as_view(), name='login'),
 ]
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,9 +1,13 @@
 from django.views import generic
 from .forms import CustomUserCreationForm
 from .models import CustomUser
-
+from django.contrib.auth.views import LoginView, LogoutView
 
 class CustomAccountCreationView(generic.CreateView):
     model = CustomUser
     form_class = CustomUserCreationForm
     template_name = 'accounts/accounts_create.html'
+    success_url = '/accounts/login'
+
+class Login(LoginView):
+    template_name = 'accounts/login.html'
