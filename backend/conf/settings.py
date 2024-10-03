96099a279415bc3067f31fcdb0665dfc2f05443c
Seidy-u <uji02seidy01@gmail.com>
2024-10-03 18:08:33 +0900

    add os.env

--- backend/conf/settings.py
+++ backend/conf/settings.py
@@ -11,6 +11,15 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+# プロジェクトのルートディレクトリにある .env ファイルをロード
+load_dotenv()
+
+# 環境変数を取得
+RAKUTEN_APP_ID = os.getenv('RAKUTEN_APP_ID')
+
+DEBUG = os.getenv('DEBUG') == 'True'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
