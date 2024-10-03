211490fa27607f072a20a64ab167e32b98c08646
Sujan Shrestha <sujanshresstha@gmail.com>
2024-10-03 11:48:04 +0900

    refractor code of login function to RESTfull api

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,7 +1,8 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from .serializers import CustomUserSerializer 
+from rest_framework.authtoken.models import Token
+from .serializers import CustomUserSerializer , LoginSerializer
     
 @api_view(['POST'])
 def create_user(request):
@@ -11,3 +12,11 @@ def create_user(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+@api_view(['POST'])
+def login(request):
+    serializer = LoginSerializer(data=request.data)
+    if serializer.is_valid():
+        token = serializer.save()['token']
+        return Response({'token': token}, status=status.HTTP_200_OK)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
