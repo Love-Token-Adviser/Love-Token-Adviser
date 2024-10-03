afdc226c0ca7f9e9aca1524396c4e7ae3fc401ca
hayamaster <dnfxmfkdbzos@naver.com>
2024-10-03 03:55:50 -0400

    feat-home page background image add

--- frontend/src/pages/HomePage/index.tsx
+++ frontend/src/pages/HomePage/index.tsx
@@ -1,23 +1,31 @@
-import { useState, useEffect } from "react";
-import apiClient from "../../apis/apiClient";
+import { Button } from "@/components/ui/button";
 
 const HomePage = () => {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      await apiClient
-        .get("LoveTokenAdviser/test/")
-        .then((res) => setData(res.data.message));
-    };
-
-    fetchData();
-  }, [setData]);
-
   return (
-    <div>
-      <h1 className="bg-red-200">Home</h1>
-      {data && <div>{data}</div>}
+    <div className="relative h-full flex justify-center items-center bg-present bg-cover">
+      <div className="absolute inset-0 bg-present bg-cover filter blur-xs" />
+      <div className="flex flex-col w-full gap-6 justify-center items-center z-10 bg-white bg-opacity-90 p-6">
+        <h1 className="text-gray-900 text-6xl font-bold">Love Token Advisor</h1>
+        <h3 className="text-gray-400 text-2xl">
+          What are you looking for Today?
+        </h3>
+        <div className="flex gap-8 w-full justify-center items-center">
+          <Button
+            variant="outline"
+            className="w-24"
+            onClick={() => console.log("clicked")}
+          >
+            Gift
+          </Button>
+          <Button
+            variant="default"
+            className="w-24"
+            onClick={() => console.log("clicked")}
+          >
+            Outfit
+          </Button>
+        </div>
+      </div>
     </div>
   );
 };
