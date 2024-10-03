d068e353cd447d13aa9fe70a86b18deeaa454318
Sujan Shrestha <sujanshresstha@gmail.com>
2024-10-03 11:59:53 +0900

    refactor code of login function to RESTfull api

--- backend/conf/settings.py
+++ backend/conf/settings.py
@@ -123,8 +123,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
-LOGIN_URL = '/accounts/login/'
-LOGIN_REDIRECT_URL = '/accounts/home/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
